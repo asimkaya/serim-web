3c1535b63f9a54b2b17f4c6d59de927db7be1ea3
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2021-10-12 17:30:59 +0300

    header anasayfa icin

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -3,13 +3,55 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  isActiveMenu0: boolean = false;
+  isActiveMenu1: boolean = false;
+  isActiveMenu2: boolean = false;
+  isActiveMenu3: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  navbarMenuClick(event: Event): void {
+    let eventTarget: Element = event.target as Element;
+    let elementId: string = eventTarget.id;
+    let elementClass: string = eventTarget.className;
+    
+    elementId == 'navbarDropdown'
+      ? (this.isActiveMenu0 = true)
+      : (this.isActiveMenu0 = false);
+    elementId == 'navbarDropdown1'
+      ? (this.isActiveMenu1 = true)
+      : (this.isActiveMenu1 = false);
+    elementId == 'navbarDropdown2'
+      ? (this.isActiveMenu2 = true)
+      : (this.isActiveMenu2 = false);
+    elementId == 'navbarDropdown3'
+      ? (this.isActiveMenu3 = true)
+      : (this.isActiveMenu3 = false);
+
+    if (elementId == 'navbarDropdown') {
+      elementClass == 'nav-link dropdown-toggle'
+        ? (this.isActiveMenu0 = false)
+        : (this.isActiveMenu0 = true);
+    }
+    if (elementId == 'navbarDropdown1') {
+      elementClass == 'nav-link dropdown-toggle'
+        ? (this.isActiveMenu1 = false)
+        : (this.isActiveMenu1 = true);
+    }
+    if (elementId == 'navbarDropdown2') {
+      elementClass == 'nav-link dropdown-toggle'
+        ? (this.isActiveMenu2 = false)
+        : (this.isActiveMenu2 = true);
+    }
+    if (elementId == 'navbarDropdown3') {
+      elementClass == 'nav-link dropdown-toggle'
+        ? (this.isActiveMenu3 = false)
+        : (this.isActiveMenu3 = true);
+    }
   }
-
 }
