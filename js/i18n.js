a0613d473bd512ae2ca04c3055c29eaf705965d4
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2021-10-21 16:03:18 +0300

    i18n altyapı tamamlandı

---
+++ js/i18n.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  const arrLang = {
+    tr: {
+      "nav-1": "Akıllı Şehir Çözümleri",
+      "nav-2": "Hizmetlerimiz",
+      "nav-3": "Hakkımızda",
+      "nav-4": "İletişim"
+    },
+
+    en: {
+      "nav-1": "Smart City Solutions",
+      "nav-2": "Our Services",
+      "nav-3": "About Us",
+      "nav-4": "Contact"
+    },
+  };
+
+  $(".dropdownMenuButtonLangItem").click(function () {
+    localStorage.setItem("language", $(this).attr("name"));
+    let lang = localStorage.getItem("language");
+
+    lang == "tr" ? $("#dropdownMenuButtonLang").text("TR") : $("#dropdownMenuButtonLang").text("EN");
+
+    $('a,h1,h2,h3,div,span,ul,button,label').each(function (index,element) {
+        $(this).text(arrLang[lang][$(this).attr('key')])
+    })
+  });
+
+
+});
