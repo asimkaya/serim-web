75abde0432edbef6afda7c28db8bece33b58ecf3
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2021-10-25 14:30:22 +0300

    sadakat sayfası tamamlandı

--- js/i18n.js
+++ js/i18n.js
@@ -4,27 +4,24 @@ $(document).ready(function () {
       "nav-1": "Akıllı Şehir Çözümleri",
       "nav-2": "Hizmetlerimiz",
       "nav-3": "Hakkımızda",
-      "nav-4": "İletişim"
+      "nav-4": "İletişim",
     },
 
     en: {
       "nav-1": "Smart City Solutions",
       "nav-2": "Our Services",
       "nav-3": "About Us",
-      "nav-4": "Contact"
+      "nav-4": "Contact",
     },
   };
 
   $(".dropdownMenuButtonLangItem").click(function () {
     localStorage.setItem("language", $(this).attr("name"));
     let lang = localStorage.getItem("language");
-
     lang == "tr" ? $("#dropdownMenuButtonLang").text("TR") : $("#dropdownMenuButtonLang").text("EN");
 
-    $('a,h1,h2,h3,div,span,ul,button,label').each(function (index,element) {
-        $(this).text(arrLang[lang][$(this).attr('key')])
-    })
+    $("a,h1,h2,h3,div,span,ul,button,label").each(function (index, element) {
+      $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
   });
-
-
 });
